afa3242733f5fb298ab437440ca8ff5c801abf55
Diana Vidal <dvidaal@gmail.com>
2023-01-23 23:39:17 +0100

    add structure folders and import/exports

---
+++ src/ts/index.ts
@@ -0,0 +1,5 @@
+import { Character } from "./characters/Character/Character";
+import { Advisor } from "./characters/Advisor/Advisor";
+import { Fighter } from "./characters/Fighter/Fighter";
+import { King } from "./characters/King/King";
+import { Squire } from "./characters/Squire/Squire";
